beed57aaa18694b9604d97df659b95edca75c9aa
Meny <m.metekia@student.fontys.nl>
2016-12-06 14:00:31 +0100

    Merge branch 'master' of https://github.com/ADS-E/Final
    Conflicts:
    	.idea/workspace.xml
    	src/webcrawler/Main.py

--- src/webcrawler/Main.py
+++ src/webcrawler/Main.py
@@ -1,16 +1,19 @@
-import CsvHelper
-from Crawler import Crawler
-from LinkFinder import LinkFinder
+import webcrawler
 import MongoHelper
+from maps import Maps
 
 def start():
-    for index in range (0,mongocollectionsize) :
-        mongodict = MongoHelper.getResultByIndex(index)
-        url = mongodict["Url"]
-        content = mongodict["Content"]
-        result = UrlResult(url)
-        spider = Spider(url, content, result)
+    for index in range(0,MongoHelper.getAvailableId()-1) :
+        site = MongoHelper.getResultByIndex(index)
+        url = site["Url"]
+        content = site["Content"]
+        result = webcrawler.UrlResult(url)
+        spider = webcrawler.Spider(url, content, result)
         result = spider.process()
         print(result)
+    end()
+
+def end():
+    Maps.start()
 
 
