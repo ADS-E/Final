d518f646883cd784c02f99e5314c1847e2454dae
Meny <m.metekia@student.fontys.nl>
2017-01-10 11:36:02 +0100

    Improved Naive bayes

--- src/ml/SetsHelper.py
+++ src/ml/SetsHelper.py
@@ -7,4 +7,9 @@ def create_sets(data):
     X = data[columns]
     y = data['Label']
 
+    X = feature_selection(X,y)
     return train_test_split(X, y, test_size=0.25, random_state=33)
+
+def feature_selection(X,y):
+    sel = SelectKBest(k=10)
+    return sel.fit_transform(X,y)
